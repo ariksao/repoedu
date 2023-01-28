11cfd14d05f8f6edbf5f82414fb9e0073754b40d
Kenny <serafimo2001@gmail.com>
2023-01-28 10:40:26 +0200

    rust calculator

--- scripting/rust/calc.rs
+++ scripting/rust/calc.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hjällöö!");
+use std::io;
+use std::io::prelude::*;
+
+fn main()
+{
+    println!("Hjälöö!");
+    println!("Wёlkömmen to rust calculator 3000 süper");
+
+    print!("Enter first number: ");
+    io::stdout().flush().unwrap(); //outputting a line without \n requires flushing stdout
+    let mut inp = String::new();
+    io::stdin().read_line(&mut inp).expect("Failed to read line");
+
+    print!("Enter second number: ");
+    io::stdout().flush().unwrap();
+    let mut inp2 = String::new();
+    io::stdin().read_line(&mut inp2).expect("Failed to read line");
+
+    println!("Result: {}", int(&inp2) + int(&inp));
+}
+
+fn int(str: &str) -> i32
+{
+    return str.trim().parse().expect("Failed to convert to integer");
 }
