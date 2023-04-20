2d339b724bf57f2b000f30e7ee78c8e3f7fdf9d3
Nikolai Serafimovich <58634778+malhwiu@users.noreply.github.com>
2023-04-20 09:47:46 +0300

    Remove int function. Move ownerships of inp and inp2 to add function instead of borrowing them, since we no longer need them in that scope

--- scripting/rust/calc.rs
+++ scripting/rust/calc.rs
@@ -10,16 +10,18 @@ fn main()
     io::stdout().flush().unwrap(); //outputting a line without \n requires flushing stdout
     let mut inp = String::new();
     io::stdin().read_line(&mut inp).expect("Failed to read line");
+    let inp = inp.trim().parse().expect("Failed to parse inp");
 
     print!("Enter second number: ");
     io::stdout().flush().unwrap();
     let mut inp2 = String::new();
     io::stdin().read_line(&mut inp2).expect("Failed to read line");
+    let inp2 = inp2.trim().parse().expect("Failed to parse inp2");
 
 	println!("Result: {}",
 		add(
-			int(&inp2),
-			int(&inp)
+			inp2,
+			inp
 		)
 	);
 }
@@ -27,9 +29,4 @@ fn main()
 fn add(val1:i32, val2:i32) -> i32
 {
 	return val1 + val2;
-}
-
-fn int(str: &str) -> i32
-{
-    return str.trim().parse().expect("Failed to convert to integer");
 }
